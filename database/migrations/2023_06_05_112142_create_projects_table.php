b935f2588ce8f2f742af8e30e38afe00835f6d56
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-06-05 16:21:20 +0200

    added sidebar to dashboard

--- database/migrations/2023_06_05_112142_create_projects_table.php
+++ database/migrations/2023_06_05_112142_create_projects_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->char('title', 60);
+            $table->char('slug', 60)->unique();
+            $table->text('description')->nullable(true);
+            $table->timestamp('project_last_update')->nullable(true);
             $table->timestamps();
         });
     }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,26 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h2 class="fs-4 text-secondary my-4">
-        {{ __('Dashboard') }}
-    </h2>
-    <div class="row justify-content-center">
-        <div class="col">
-            <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
+    <div class="mc-wrapper d-flex">
 
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
+        <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-dark navbar-dark sidebar collapse ">
+            <div class="position-sticky pt-3">
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        asasas
+                        {{-- <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.dashboard' ? 'bg-secondary' : '' }}"
+                    href="{{ route('admin.dashboard') }}">
+                    <i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Dashboard
+                </a> --}}
+                    </li>
+                    <li class="nav-item">
+                        saasassa
+                        {{-- <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.posts.index' ? 'bg-secondary' : '' }}"
+                href="{{ route('admin.posts.index') }}">
+                <i class="fa-regular fa-folder-open"></i> Post
+            </a> --}}
+                    </li>
+                </ul>
+
+
+            </div>
+        </nav>
+
+        <div class="container">
+            <h2 class="fs-4 text-secondary my-4">
+                {{ __('Dashboard') }}
+            </h2>
+            <div class="row justify-content-center">
+                <div class="col">
+                    <div class="card">
+                        <div class="card-header">Dashboard di {{ Auth::user()->name }}</div>
+
+                        <div class="card-body">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
+
+                            {{ __('You are logged in!') }}
+                        </div>
                     </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
