c41dc2613f58c1ee12531f17f44c049b0a1cd987
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-06-05 17:34:25 +0200

    added: projects.show.blade

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -2,18 +2,23 @@
 
 @section('content')
     <div class="container d-flex align-items-center justify-content-center h-100 w-100">
+
         <div class="card" style="width: 18rem;">
             <div class="card-body">
                 <h5 class="card-title">{{ $project->title }}</h5>
-                <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                <h6 class="card-subtitle mb-2 text-body-secondary">Url: {{ $project->slug }}</h6>
                 <p class="card-text">{{ $project->description }}</p>
-                <a href="#" class="card-link">Card link</a>
-                <form action="{{ route('admin.projects.destroy', $project->slug) }}" method="POST">
-                    @csrf
-                    @method('DELETE')
+                <div class="d-flex">
 
-                    <button type="submit" class="btn btn-danger"><i class="fa-solid fa-trash-can"></i></button>
-                </form>
+                    <a href="{{ route('admin.projects.index') }}" class="btn btn-warning"><i
+                            class="fa-solid fa-arrow-left"></i></a>
+                    <form class="ms-2" action="{{ route('admin.projects.destroy', $project->slug) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+
+                        <button type="submit" class="btn btn-danger"><i class="fa-solid fa-trash-can"></i></button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
