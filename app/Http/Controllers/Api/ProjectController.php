c4d332eb1463394c1f55e49eb0ea11e37986ff8d
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-06-17 10:48:20 +0200

    added api/TypeCOntroller

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -8,15 +8,29 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public function index() {
-        $projects = Project::with('type','technologies')->paginate(8);
+    public function index(Request $request)
+    {
+
+        $filter = $request->all();
+
+
+        if ($request->has('type_id')) {
+            $projects = Project::with('type', 'technologies')->where('type_id', $filter['type_id'])->paginate(8);
+        } else {
+            $projects = Project::with('type', 'technologies')->paginate(8);
+        };
+
+
         return response()->json([
             'response' => 'success',
             'results' => $projects
         ]);
     }
-    public function show($slug) {
-        $project = Project::with('type','technologies')->where('slug', $slug)->first();
+
+
+    public function show($slug)
+    {
+        $project = Project::with('type', 'technologies')->where('slug', $slug)->first();
         return response()->json([
             'success' => true,
             'results' => $project
---
+++ app/Http/Controllers/Api/TypeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Type;
+use Illuminate\Http\Request;
+
+class TypeController extends Controller
+{
+    public function index()
+    {
+        $types = Type::all();
+        return response()->json([
+            'response' => 'success',
+            'results' => $types
+        ]);
+    }
+}
